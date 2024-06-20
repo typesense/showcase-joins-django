b48443dbb8eddb7dad83708f90dde009b48369fb
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-06-20 16:38:25 +0300

    feat(fe): add util function for react router error

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,21 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { isRouteErrorResponse } from "react-router-dom";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function errorMessage(error: unknown): string {
+  switch (true) {
+    case isRouteErrorResponse(error):
+      return `${error.status}: ${error.statusText}`;
+    case error instanceof Error:
+      return error.message;
+    case typeof error === "string":
+      return error;
+    default:
+      console.error(error);
+      return "Unknown error";
+  }
 }
