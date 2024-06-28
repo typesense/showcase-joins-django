d7388f333eb978d4a53eef499f6b73d516ad51f9
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-06-28 13:12:22 +0300

    feat(fe): add root layout

---
+++ frontend/src/routes/layout.tsx
@@ -0,0 +1,23 @@
+import { Home, ShieldHalf, User } from "lucide-react";
+import { MainNav } from "@/components/main-nav";
+import { Header } from "@/components/header";
+import { Outlet } from "react-router-dom";
+import type { NavItemProps } from "@/components/sidenav";
+import { Icons } from "@/components/icons";
+
+const items: NavItemProps[] = [
+];
+
+const RootLayout = () => (
+  <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[220px_1fr]">
+    <MainNav items={items} />
+    <div className="flex flex-col">
+      <Header items={items} />
+      <main className="flex flex-1 flex-col gap-4  lg:gap-6 p-4">
+        <Outlet />
+      </main>
+    </div>
+  </div>
+);
+
+export default RootLayout;
