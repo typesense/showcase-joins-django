98462aa63476cd00c25dad678c8113926c25d569
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-06-28 13:53:21 +0300

    feat: config django rest framework

--- f1/settings.py
+++ f1/settings.py
@@ -48,7 +48,16 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    "DEFAULT_PERMISSION_CLASSES": [
+        # "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+    ]
+}
 
 MIDDLEWARE = [
     "results.middleware.TypesenseClientMiddleware",
