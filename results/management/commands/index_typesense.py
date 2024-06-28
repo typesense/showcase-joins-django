e4ce43bd364fff9eb0a4ced427156c6b5484f7b8
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-06-28 17:16:33 +0300

    fix: use common util func for indexing typesense

--- results/management/commands/index_typesense.py
+++ results/management/commands/index_typesense.py
@@ -4,44 +4,13 @@ from django.apps import apps
 from django.core.management.base import BaseCommand
 from django.db import models
 
+from common.util.typesense_utils import instance_to_document
+
 
 class Command(BaseCommand):
     help = "Indexes the Typesense client for every model in the app"
 
     def handle(self, *args, **options):
-
-        def instance_to_document(instance):
-
-            def format_value(field, value):
-                formats = {
-                    models.DateField: lambda v: (
-                        int(datetime.datetime(v.year, v.month, v.day).timestamp())
-                        if v
-                        else None
-                    ),
-                    models.DateTimeField: lambda v: (
-                        int(
-                            datetime.datetime.strptime(v, "%Y-%m-%d").date().timestamp()
-                        )
-                        if v
-                        else None
-                    ),
-                    models.TimeField: lambda v: v.isoformat() if v else None,
-                    models.ForeignKey: lambda v: str(v.id) if v else None,
-                }
-
-                for field_type, callback in formats.items():
-                    if isinstance(field, field_type):
-                        return callback(value)
-
-                if field.name == "id":
-                    return str(value)
-                return value
-
-            return {
-                field.name: format_value(field, getattr(instance, field.name))
-                for field in instance._meta.fields
-            }
 
         # Get the app config
         app_config = apps.get_app_config("results")
