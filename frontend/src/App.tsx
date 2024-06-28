6bb7067e064393227a648ba948ce5be731542085
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-06-28 13:18:26 +0300

    feat(fe): register home page to router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,10 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <ErrorPage />,
+  },
 ]);
 
 function App() {
